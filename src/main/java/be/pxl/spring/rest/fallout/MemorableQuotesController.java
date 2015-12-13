7551e480025e289b5d33dd8507929e8eed85971c
Sch3lp <sch3lp@gmail.com>
2015-12-13 17:56:15 +0100

    poging to integrationtest

--- src/main/java/be/pxl/spring/rest/fallout/MemorableQuotesController.java
+++ src/main/java/be/pxl/spring/rest/fallout/MemorableQuotesController.java
@@ -1,27 +1,34 @@
 package be.pxl.spring.rest.fallout;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RequestMapping(MemorableQuotesController.QUOTE_BASE_URL)
 @RestController
 public class MemorableQuotesController {
 
+    public static final String QUOTE_BASE_URL = "/quote/";
     private static final List<Quote> quotes;
 
     static {
         quotes = new ArrayList<>();
-        quotes.add(Quote.of(" Narrator", "War...War never changes"));
-        quotes.add(Quote.of(" Mr. Fantastic", "They asked me how well I understood theoretical physics. I said I had a theoretical degree in physics. They said welcome aboard"));
-        quotes.add(Quote.of(" Liberty Prime","Freedom is the sovereign right of every American"));
-        quotes.add(Quote.of(" Thomas Hildern","Too many people have opnions on things they know nothing about. And the more ignorant they are, the more opinions they have"));
+        quotes.add(Quote.of("Narrator", "War...War never changes"));
+        quotes.add(Quote.of("Mr. Fantastic", "They asked me how well I understood theoretical physics. I said I had a theoretical degree in physics. They said welcome aboard"));
+        quotes.add(Quote.of("Liberty Prime","Freedom is the sovereign right of every American"));
+        quotes.add(Quote.of("Thomas Hildern","Too many people have opnions on things they know nothing about. And the more ignorant they are, the more opinions they have"));
         quotes.add(Quote.of("Moira Brown", "Here, take a few radiation chems, as my little way of saying, \"I'm sorry I twisted your DNA like a kitten with a ball of yarn.\""));
     }
 
     @RequestMapping("/")
     public List<Quote> all(){
+        return quotes;
+    }
+
+    public List<Quote> queried(){
         return quotes;
     }
 }
---
+++ src/test/java/be/pxl/spring/rest/fallout/MemorableQuotesControllerTest.java
@@ -0,0 +1,72 @@
+package be.pxl.spring.rest.fallout;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+@WebAppConfiguration
+public class MemorableQuotesControllerTest {
+
+    // Spring-test utility class that queries Spring's DispatcherServlet to assert
+    private MockMvc mockMvc;
+
+    private HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+    @Autowired
+    private MemorableQuotesController ctrlr;
+    @Autowired
+    private WebApplicationContext webAppContext;
+
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+        mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream().filter(
+                hmc -> hmc instanceof MappingJackson2HttpMessageConverter).findAny().get();
+
+        assertThat(mappingJackson2HttpMessageConverter)
+                .isNotNull()
+                .describedAs("the JSON message converter must not be null");
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+
+    }
+
+    @Test
+    public void all_ListsAllQuotes() throws Exception {
+        mockMvc.perform(get(MemorableQuotesController.QUOTE_BASE_URL))
+                .andExpect(status().isOk())
+//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(asJson(Quote.of("Narrator", "War...War never changes"))));
+    }
+
+    protected String asJson(Object o) throws IOException {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        this.mappingJackson2HttpMessageConverter.write(
+                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
+    }
+}
