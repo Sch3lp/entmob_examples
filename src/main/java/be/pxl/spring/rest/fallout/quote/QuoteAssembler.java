bdff194d2f8132fbfeb30d46ef6dde71632a84ca
Sch3lp <sch3lp@gmail.com>
2015-12-19 19:04:10 +0100

    added comments to clarify + annotated repository to hint IntelliJ at SpringBoots magic wiring

--- src/main/java/be/pxl/spring/rest/fallout/quote/QuoteAssembler.java
+++ src/main/java/be/pxl/spring/rest/fallout/quote/QuoteAssembler.java
@@ -1,5 +1,6 @@
 package be.pxl.spring.rest.fallout.quote;
 
+// Example of not using @Service or @Component, see be.pxl.spring.rest.fallout.Application for this class' configuration
 public class QuoteAssembler {
     public QuoteR toRepresentation(Quote quote) {
         return QuoteR.of(quote.getId().toString(), quote.getAuthor(), quote.getQuotation());
--- src/main/java/be/pxl/spring/rest/fallout/quote/QuoteRepository.java
+++ src/main/java/be/pxl/spring/rest/fallout/quote/QuoteRepository.java
@@ -1,9 +1,11 @@
 package be.pxl.spring.rest.fallout.quote;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public interface QuoteRepository extends JpaRepository<Quote, UUID> {
 
 }
