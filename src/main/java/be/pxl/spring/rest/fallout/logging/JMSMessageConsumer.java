468eef3202525272bf12c4558c5c94e9bf92649b
Sch3lp <sch3lp@gmail.com>
2016-01-19 22:20:31 +0100

    Added TODO

--- src/main/java/be/pxl/spring/rest/fallout/logging/JMSMessageConsumer.java
+++ src/main/java/be/pxl/spring/rest/fallout/logging/JMSMessageConsumer.java
@@ -10,6 +10,7 @@ public class JMSMessageConsumer {
 
     @JmsListener(destination = "ItemLogQueue")
     public void onMessage(Message message) {
+        //TODO: save to log table or something
         System.out.println("Hi, I'm JMSMessageConsumer and I got " + message);
     }
 }
