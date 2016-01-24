ee76e160c6fa29e5b3f58504d1bac5c88e7bbcea
Sch3lp <sch3lp@gmail.com>
2016-01-24 16:20:39 +0100

    optimized imports

--- src/main/java/be/pxl/spring/rest/fallout/Application.java
+++ src/main/java/be/pxl/spring/rest/fallout/Application.java
@@ -1,16 +1,12 @@
 package be.pxl.spring.rest.fallout;
 
 import be.pxl.spring.rest.fallout.quote.QuoteAssembler;
-import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.core.support.JmsGatewaySupport;
 
-import javax.jms.ConnectionFactory;
-import javax.jms.Destination;
 import javax.jms.Queue;
 
 /*
--- src/main/java/be/pxl/spring/rest/fallout/quote/QuoteRepository.java
+++ src/main/java/be/pxl/spring/rest/fallout/quote/QuoteRepository.java
@@ -3,7 +3,6 @@ package be.pxl.spring.rest.fallout.quote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
